a1c07049574cc9bdfccac9681268f83717887868
dillonteachescoding <dillon.kierce@greenriver.edu/bin/python3>
2025-10-16 16:12:05 -0700

    enwstuff

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
-# By 
+# By Dillon ping? importing functions
+
+#Libraries
+import platform
+import os
+
+ip = input("Please enter a valid IP address with x.x.x.x: ")
+
+#Build a ping command
+ping_cmd = f"ping -c 1 -w 2 {ip}"
+
+#Exectue command and capture output
+exit_code = os.system(ping_cmd)
+
+#Print the ouput
+print(exit_code)
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Dillon Kierce
+
+#Hardcoded variables
+
+questions = [
+    "What is your name? ",
+    "What is your favorite color",
+    "What is your city you grew up in?",
+    "What is the current season?"
+]
+
+answer = []
+
+
+# Create a loop that will take question and save it to a file that we can read
+for q in questions:
+    ans = input(f"Question {q}: ")
+    answer.append(f"{ans}")
+
+
+with open ("hackme.txt", "w") as file:
+    for line in answer:
+        file.write(line + "\n")
+
+
+with open ("hackme.txt", "r") as file:
+    line = file.read()
+    print(line)
